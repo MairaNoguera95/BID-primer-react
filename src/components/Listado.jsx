9225594d36b82d0990d81de646dee2018de1b543
Maira Noguera <mai09sol@gmail.com>
2023-01-25 02:34:41 -0300

    Primer React Core

---
+++ src/components/Listado.jsx
@@ -0,0 +1,15 @@
+import { Component} from "react";
+class Listado extends Component {
+render() {
+    return( <div>
+        <ul>
+            <li> Learn React</li>
+            <li> Climb Mt. Everest</li>
+            <li> Run a marathon</li>
+            <li> Feed the dogs</li>
+        </ul>
+    </div>);
+
+}
+}
+export default Listado;
---
+++ src/components/Subtitulo.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Subtitulo extends Component{
+    render(){
+        return(<div>
+            <h2>Things I need to do:</h2>
+        </div>
+
+        );
+    }
+}
+export default Subtitulo;
